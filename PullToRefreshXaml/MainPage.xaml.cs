a30c3047449e7f3b1d3707492b7b0e1d7ee62981
Justin Liu <justin.xin.liu@gmail.com>
2015-12-15 09:49:45 +1100

    Calculate the time between the list gets pulled all the way down & rolled back up to determine is the refresh should be cancelled or not.

--- PullToRefreshXaml/MainPage.xaml.cs
+++ PullToRefreshXaml/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Numerics;
+using System.Threading.Tasks;
 using Windows.UI.Composition;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Hosting;
@@ -21,7 +22,9 @@ namespace PullToRefreshXaml
         Visual _refreshIconVisual;
         float _refreshIconOffsetY;
         const float REFRESH_ICON_MAX_OFFSET_Y = 12.0f;
+
         bool _refresh;
+        DateTime _pulledDownTime, _restoredTime;
 
         public MainPage()
         {
@@ -31,13 +34,20 @@ namespace PullToRefreshXaml
             this.Loaded += (s, e) =>
             {
                 _scrollViewer = ListView.GetScrollViewer();
-                _scrollViewer.ViewChanged += ScrollViewer_ViewChanged;
                 _scrollViewer.DirectManipulationStarted += OnDirectManipulationStarted;
                 _scrollViewer.DirectManipulationCompleted += OnDirectManipulationCompleted;
 
                 // Retrieve the ScrollViewer manipulation and the Compositor.
                 _scrollerViewerManipulation = ElementCompositionPreview.GetScrollViewerManipulationPropertySet(_scrollViewer);
                 var compositor = _scrollerViewerManipulation.Compositor;
+
+                // At the moment there are three things happening when pulling down the list -
+                // 1. The Refresh Icon fades in.
+                // 2. The Refresh Icon rotates (400°).
+                // 3. The Refresh Icon gets pulled down a bit (REFRESH_ICON_MAX_OFFSET_Y)
+                // QUESTION 5
+                // Can we also have Geometric Path animation so we can also draw the Refresh Icon along the way?
+                //
 
                 // Create a rotation expression animation based on the overpan distance of the ScrollViewer.
                 _rotationAnimation = compositor.CreateExpressionAnimation("min(max(0, ScrollManipulation.Translation.Y) * Multiplier, MaxDegree)");
@@ -69,38 +79,41 @@ namespace PullToRefreshXaml
 
             this.Unloaded += (s, e) =>
             {
-                _scrollViewer.ViewChanged -= ScrollViewer_ViewChanged;
                 _scrollViewer.DirectManipulationStarted -= OnDirectManipulationStarted;
                 _scrollViewer.DirectManipulationCompleted -= OnDirectManipulationCompleted;
             };
         }
 
         void OnDirectManipulationStarted(object sender, object e)
-        { 
+        {
             // QUESTION 1
             // I cannot think of a better way to monitor overpan changes, maybe there should be an Animating event?
             //
             Windows.UI.Xaml.Media.CompositionTarget.Rendering += OnCompositionTargetRendering;
 
-            // Initialise the refresh flag.
+            // Initialise the values.
             _refresh = false;
-        }
-
-        /// <summary>
-        /// Detach the event as ViewChanged is not invoked when overpaning.
-        /// </summary>
-        void ScrollViewer_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
-        {
-            Windows.UI.Xaml.Media.CompositionTarget.Rendering -= OnCompositionTargetRendering;
         }
 
         void OnDirectManipulationCompleted(object sender, object e)
         {
             Windows.UI.Xaml.Media.CompositionTarget.Rendering -= OnCompositionTargetRendering;
 
+            //Debug.WriteLine($"ScrollViewer Rollback animation duration: {(_restoredTime - _pulledDownTime).Milliseconds}");
+
+            // The ScrollViewer's rollback animation is appx. 200ms. So if the duration between the two DateTimes we recorded earlier
+            // is greater than 250ms, we should cancel the refresh.
+            var cancelled = (_restoredTime - _pulledDownTime) > TimeSpan.FromMilliseconds(250);
+
             if (_refresh)
             {
-                Debug.WriteLine("Refresh now!!? Wait a sec, this might not always be the case...");
+                if (cancelled)
+                {
+                    Debug.WriteLine("Refresh cancelled...");
+                }
+                else {
+                    Debug.WriteLine("Refresh now!!!");
+                }
             }
         }
 
@@ -133,12 +146,27 @@ namespace PullToRefreshXaml
 
             // Question 4
             // It's not always the case here as the user can pull it all the way down and then push it back up to
-            // CANCEL a refresh!! Though I cannot seem to find an easy (non-hacky) way to detect right after the 
-            // finger is lifted. DirectManipulationCompleted is called too late.
+            // CANCEL a refresh!! Though I cannot seem to find an easy way to detect right after the finger is lifted.
+            // DirectManipulationCompleted is called too late.
+            // What might be really helpful is to have a DirectManipulationDelta event with velocity and other values.
+            //
+            // At the moment I am calculating the time difference between the list gets pulled all the way down and rolled back up.
             // 
             if (!_refresh)
             {
                 _refresh = _refreshIconOffsetY == REFRESH_ICON_MAX_OFFSET_Y;
+            }
+
+            if (_refreshIconOffsetY == REFRESH_ICON_MAX_OFFSET_Y)
+            {
+                _pulledDownTime = DateTime.Now;
+                //Debug.WriteLine($"When the list is pulled down: {_pulledDownTime}");
+            }
+
+            if (_refresh && _refreshIconOffsetY <= 1)
+            {
+                _restoredTime = DateTime.Now;
+                //Debug.WriteLine($"When the list is back up: {_restoredTime}");
             }
 
             _refreshIconVisual.StartAnimation("Offset.Y", _offsetAnimation);
