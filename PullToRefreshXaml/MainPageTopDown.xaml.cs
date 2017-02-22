8f0427e826eb39a2a1653850cb0c61fd56d4e1f7
Justin <justin@drawboard.com>
2017-02-22 23:25:56 +1100

    Changed to create a new storyboard instance each time in case multi items have been added.

--- PullToRefreshXaml/MainPageTopDown.xaml.cs
+++ PullToRefreshXaml/MainPageTopDown.xaml.cs
@@ -13,7 +13,6 @@ namespace PullToRefreshXaml
     public sealed partial class MainPageTopDown : Page
     {
         private readonly ObservableCollection<GroupInfoList> _list = Contact.GetContactsGrouped(250);
-        private Tuple<Timeline, Storyboard> _colorAnimation;
 
         public MainPageTopDown()
         {
@@ -26,7 +25,6 @@ namespace PullToRefreshXaml
         private async void OnLoaded(object sender, RoutedEventArgs e)
         {
             await Task.Delay(1000);
-            _colorAnimation = CreateAnimation();
             ListView.ContainerContentChanging += OnListViewContainerContentChanging;
         }
 
@@ -43,39 +41,27 @@ namespace PullToRefreshXaml
         {
             if (args.ItemContainer != null && !args.InRecycleQueue && args.Phase == 0)
             {
-                _colorAnimation.Item2.Stop();
-                Storyboard.SetTarget(_colorAnimation.Item1, args.ItemContainer);
-                _colorAnimation.Item2.Begin();
+                var colorAnimation = new ColorAnimationUsingKeyFrames
+                {
+                    // 'cause the new item comes in with an animation of which duration is about 300s, we add a little delay here to only
+                    // animate the color after it appears.
+                    BeginTime = TimeSpan.FromMilliseconds(300)
+                };
+                var keyFrame1 = new LinearColorKeyFrame { KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(0)), Value = Colors.White };
+                var keyFrame2 = new LinearColorKeyFrame { KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(400)), Value = Colors.LightGray };
+                var keyFrame3 = new LinearColorKeyFrame { KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(1200)), Value = Colors.White };
+                colorAnimation.KeyFrames.Add(keyFrame1);
+                colorAnimation.KeyFrames.Add(keyFrame2);
+                colorAnimation.KeyFrames.Add(keyFrame3);
+
+                ListView.Background = new SolidColorBrush(Colors.Transparent);
+                Storyboard.SetTarget(colorAnimation, args.ItemContainer);
+                Storyboard.SetTargetProperty(colorAnimation, "(Control.Background).(SolidColorBrush.Color)");
+
+                var storyboard = new Storyboard();
+                storyboard.Children.Add(colorAnimation);
+                storyboard.Begin();
             }
-        }
-
-        /// <summary>
-        /// As creating Timeline and Storyboard can be expensive, we only create one instance for each, and basically each
-        /// time just swap the Target of the Storyboard in `Storyboard.SetTarget(_colorAnimation.Item1, args.ItemContainer);`
-        /// </summary>
-        /// <returns>Returns a Tuple of a Timeline and a Storbyard.</returns>
-        private Tuple<Timeline, Storyboard> CreateAnimation()
-        {
-            var colorAnimation = new ColorAnimationUsingKeyFrames
-            {
-                // 'cause the new item comes in with an animation of which duration is about 300s, we add a little delay here to only
-                // animate the color after it appears.
-                BeginTime = TimeSpan.FromMilliseconds(300)
-            };
-            var keyFrame1 = new LinearColorKeyFrame { KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(0)), Value = Colors.White };
-            var keyFrame2 = new LinearColorKeyFrame { KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(400)), Value = Colors.LightGray };
-            var keyFrame3 = new LinearColorKeyFrame { KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(1200)), Value = Colors.White };
-            colorAnimation.KeyFrames.Add(keyFrame1);
-            colorAnimation.KeyFrames.Add(keyFrame2);
-            colorAnimation.KeyFrames.Add(keyFrame3);
-
-            ListView.Background = new SolidColorBrush(Colors.Transparent);
-            Storyboard.SetTargetProperty(colorAnimation, "(Control.Background).(SolidColorBrush.Color)");
-
-            var storyboard = new Storyboard();
-            storyboard.Children.Add(colorAnimation);
-
-            return new Tuple<Timeline, Storyboard>(colorAnimation, storyboard);
         }
     }
 }
